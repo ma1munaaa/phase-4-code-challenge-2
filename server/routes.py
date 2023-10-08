5f7df13ccd29ed506c459b51909e569b09a25027
ma1munaaa <135684307+ma1munaaa@users.noreply.github.com>
2023-10-08 09:50:21 +0300

    Delete server directory

--- server/routes.py
+++
@@ -1,5 +0,0 @@
-# from app import api,make_response,jsonify,app
-# from flask import request
-# from flask_restful import Resource
-# from api.models import Restaurant,RestaurantPizza,Pizza,db,SerializerMixin
-
